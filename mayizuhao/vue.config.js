bbd278bcffa426fd3a71c4185643cc02aa8a9ca6
rengl <874670332@qq.com>
2019-11-26 16:14:41 +0800

    ...

--- mayizuhao/vue.config.js
+++ mayizuhao/vue.config.js
@@ -1,3 +1,13 @@
+// 获取命令行指令
+const configArgv = JSON.parse(process.env.npm_config_argv);
+console.log(configArgv)
+// 截取
+const original = configArgv.original.slice(1);
+console.log(original)
+// 截取参数
+const stage = original[1] ? original[1].replace(/-/g, "") : "dev";
+console.log(stage)
+
 module.exports = {
 	productionSourceMap: false
 }
