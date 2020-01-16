b4e3b866c337a910553a33fe01ead9f4fae837a9
rengl <874670332@qq.com>
2020-01-16 16:09:35 +0800

    ...

--- mayizuhao/src/components/null-data/index.js
+++
@@ -1,9 +0,0 @@
-import nullDataComponent from './null-data.vue';
-
-const nullData = {
-    install (Vue) {
-        Vue.component('nullData', nullDataComponent)
-    }
-}
-
-export default nullData;
